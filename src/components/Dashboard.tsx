d1ea5bcc19d6cff407b434ffa353caf280c64027
Afonso Lima <afonsolima@gmail.com>
2025-03-17 18:07:30 -0300

    dashboard layout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,19 +1,12 @@
+import DashboardCard from "./DashboardCard";
+
 export default function Dashboard() {
   return (
     <div className="flex flex-col w-full max-w-[1120px] h-32 ">
       <div className="flex w-full h-16 items-center justify-between gap-5 ">
-        <div className="flex flex-col h-36 items-start justify-evenly p-4 bg-gray-700 text-white w-full">
-          <p>Entradas</p>
-          <h2>R$ 1.234,56</h2>
-        </div>
-        <div className="flex flex-col h-36 items-start justify-evenly p-4 bg-gray-700 text-white w-full">
-          <p>Saídas</p>
-          <h2>R$ 1.234,56</h2>
-        </div>
-        <div className="flex flex-col h-36 items-start justify-evenly p-4 bg-green-700 text-white w-full">
-          <p>Total</p>
-          <h2>R$ 1.234,56</h2>
-        </div>
+        <DashboardCard type="IN" value={1245.67} />
+        <DashboardCard type="OUT" value={1245.67} />
+        <DashboardCard type="TOT" value={1245.67} />
       </div>
     </div>
   );
---
+++ src/components/DashboardCard.tsx
@@ -0,0 +1,39 @@
+import { ArrowCircleDown, ArrowCircleUp, Money } from "@phosphor-icons/react";
+
+export default function DashboardCard({
+  type,
+  value,
+}: {
+  type: "IN" | "OUT" | "TOT";
+  value: number;
+}) {
+  let bgColor = "bg-gray-700";
+  let title = "Entradas";
+  let icon = <ArrowCircleUp size={32} color="green" />;
+
+  switch (type) {
+    case "IN":
+      break;
+    case "OUT":
+      title = "Saídas";
+      icon = <ArrowCircleDown size={32} color="red" />;
+      break;
+    case "TOT":
+      title = "Total";
+      icon = <Money size={32} color="white" />;
+      bgColor = "bg-green-700";
+      break;
+  }
+
+  return (
+    <div
+      className={`flex flex-col h-36 items-start justify-evenly px-4 ${bgColor} text-white w-full rounded`}
+    >
+      <div className="flex w-full items-center justify-between">
+        <p>{title}</p>
+        {icon}
+      </div>
+      <h2>{value}</h2>
+    </div>
+  );
+}
