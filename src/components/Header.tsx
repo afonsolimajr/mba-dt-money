26557b4f3c9366a2b17c9c3ec8d11fcbb2a642d8
Afonso Lima <afonsolima@gmail.com>
2025-03-18 17:07:44 -0300

    NewTransactionModal layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import { HandCoins } from "@phosphor-icons/react";
+import NewTransactionModal from "./NewTransactionModal";
 
 export default function Header() {
   return (
     <div className="flex flex-col w-full max-w-[1120px] h-14 bg-gray-950 pt-4">
       <div className="flex w-full items-center justify-between">
-        <div className="flex gap-3">
+        <div className="flex gap-3 items-center">
           <HandCoins size={32} color="green" />
           <h2>DT Money</h2>
         </div>
-        <button className="bg-green-700 ">Nova Transação</button>
+        <NewTransactionModal />
       </div>
     </div>
   );
---
+++ src/components/NewTransactionModal.tsx
@@ -0,0 +1,92 @@
+import { ArrowCircleDown, ArrowCircleUp } from "@phosphor-icons/react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+
+export default function NewTransactionModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default" className="bg-green-700 ">
+          Nova Transação
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] bg-gray-900">
+        <DialogHeader>
+          <DialogTitle>Nova Transação</DialogTitle>
+          <DialogDescription>Adiciona uma nova transação.</DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col w-full gap-5 py-4 ">
+          <Input
+            id="description"
+            defaultValue=""
+            placeholder="Descrição"
+            className="w-full bg-gray-950 border-none"
+          />
+          <Input
+            id="price"
+            defaultValue=""
+            placeholder="preço"
+            className="w-full bg-gray-950 border-none"
+          />
+          <Input
+            id="category"
+            defaultValue=""
+            placeholder="Categoria"
+            className="w-full bg-gray-950 border-none"
+          />
+        </div>
+        <div className="flex w-full">
+          <RadioGroup
+            defaultValue="option-one"
+            className="flex w-full items-center justify-center"
+          >
+            <div className="flex w-full items-center">
+              <Label
+                htmlFor="option-one"
+                className="w-full justify-center bg-gray-800 cursor-pointer rounded-md p-2 flex items-center gap-2 [&:has(:checked)]:bg-zinc-100 dark:[&:has(:checked)]:bg-zinc-800"
+              >
+                <RadioGroupItem
+                  value="option-one"
+                  id="option-one"
+                  className="hidden"
+                />
+                <ArrowCircleUp size={24} color="green" />
+                Entrada
+              </Label>
+            </div>
+            <div className="flex w-full items-center">
+              <Label
+                htmlFor="option-two"
+                className="w-full justify-center bg-gray-800 cursor-pointer rounded-md p-2 flex items-center gap-2 [&:has(:checked)]:bg-zinc-100 dark:[&:has(:checked)]:bg-zinc-800"
+              >
+                <RadioGroupItem
+                  value="option-two"
+                  id="option-two"
+                  className="hidden"
+                />
+                <ArrowCircleDown size={24} color="red" />
+                Saída
+              </Label>
+            </div>
+          </RadioGroup>
+        </div>
+        <DialogFooter>
+          <Button type="submit" className="bg-green-800 w-full">
+            Cadastrar
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
