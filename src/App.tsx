ff33e07ff605f4c7d489403618abde62a97fb8ba
Afonso Lima <afonsolima@gmail.com>
2025-03-18 17:57:51 -0300

    requesting transactions data with fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Background from "./components/Background";
 import Search from "./components/Search";
 import TransactionsPanel from "./components/TransactionsPanel";
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  async function LoadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    LoadTransactions();
+  }, []);
+
+  useEffect(() => {
+    console.log(transactions);
+  }, [transactions]);
+
   return (
     <div className="flex flex-col items-center w-full">
       <div className="flex flex-col items-center justify-start w-full h-full gap-2">
