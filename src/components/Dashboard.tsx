d702c4e803fd8955e93b6e1f00881fb0d6dd66e3
Afonso Lima <afonsolima@gmail.com>
2025-03-19 15:26:37 -0300

    dashboard summary with reduce

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,34 @@
+import { useContext } from "react";
 import DashboardCard from "./DashboardCard";
+import { TransactionsContext } from "@/contexts/TransactionsContext";
 
 export default function Dashboard() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
   return (
     <div className="flex flex-col w-full max-w-[1120px] h-32 ">
       <div className="flex w-full h-16 items-center justify-between gap-5 ">
-        <DashboardCard type="IN" value={1245.67} />
-        <DashboardCard type="OUT" value={1245.67} />
-        <DashboardCard type="TOT" value={1245.67} />
+        <DashboardCard type="IN" value={summary.income} />
+        <DashboardCard type="OUT" value={summary.outcome} />
+        <DashboardCard type="TOT" value={summary.total} />
       </div>
     </div>
   );
