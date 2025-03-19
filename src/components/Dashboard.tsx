bb41db85b3eb3e0d0e3ebc39fb3c34b097c25902
Afonso Lima <afonsolima@gmail.com>
2025-03-19 15:35:44 -0300

    summary hook

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,28 +1,9 @@
-import { useContext } from "react";
 import DashboardCard from "./DashboardCard";
-import { TransactionsContext } from "@/contexts/TransactionsContext";
+import { useSummary } from "@/hooks/useSummary";
 
 export default function Dashboard() {
-  const { transactions } = useContext(TransactionsContext);
+  const summary = useSummary();
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
   return (
     <div className="flex flex-col w-full max-w-[1120px] h-32 ">
       <div className="flex w-full h-16 items-center justify-between gap-5 ">
